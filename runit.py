27d4dd36242aa31364544cb93227bf84fd5f368c
Stewart Rutledge <stewart.rutledge@bonniernews.se>
2017-11-07 16:07:32 +0100

    Initial

---
+++ runit.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostPort = 8000
+hostName = '0.0.0.0'
+
+class RequestHandler(BaseHTTPRequestHandler):
+	html_response = """
+	<h1>Hello and welcome!</h1>
+	<p>Thank you for being here, and viewing me on %s</p>
+	<hr/>
+	<h4>You are welcome to visit me again on any path you like, maybe: <a href="/here/and/there"> here?</a></h4>
+	"""
+
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		self.wfile.write(bytes(self.html_response % self.path, "utf-8"))
+
+
+
+def run(server_class=HTTPServer, handler_class=RequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+run()
