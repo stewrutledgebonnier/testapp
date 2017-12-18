ddcc0e6661292cfada39436b2c4b6c2ac3467ad9
Stewart Rutledge <stewart.rutledge@bonniernews.se>
2017-12-18 10:32:23 +0100

    osdo

--- runit.py
+++ runit.py
@@ -3,31 +3,42 @@
 import socket
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import logging
+
 
 hostPort = 8000
 hostName = '0.0.0.0'
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - TestApp - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 class RequestHandler(BaseHTTPRequestHandler):
-	html_response = """
-	<h1>Hello and welcome!</h1>
-	<p>Thank you for being here, and viewing me on %s</p>
-	<hr/>
-	<h4>You are welcome to visit me again on any path you like, maybe: <a href="/here/and/there"> here?</a></h4>
-	<h1>TIMES TWO WOW! I work well</h1>
-	"""
+    html_response = """
+        <h1 style="color: red;">Glad jul!</h1>
+    """
 
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
-		self.wfile.write(bytes(self.html_response % self.path, "utf-8"))
-		print("SUPERLOG[INFO]:::::::Served path: %s" % self.path)
+    def do_GET(self):
+        logger.debug("Processing request")
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(self.html_response, "utf-8"))
+        logger.info("Served path: %s" % self.path)
+        with open("tree.svg", "rb") as tree:
+            self.wfile.write(tree.read())
 
 
 
 def run(server_class=HTTPServer, handler_class=RequestHandler):
+    logger.info("Starting server")
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
+    logger.info("Serving from port 8000")
     httpd.serve_forever()
 
-run()
+while True:
+    run()
