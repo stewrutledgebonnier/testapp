dfe4bf663ddef676a5d3c84e484591eb8544e175
Stewart Rutledge <stewart.rutledge@bonniernews.se>
2018-06-29 09:59:17 +0200

    Added stuff

--- runit.py
+++ runit.py
@@ -24,6 +24,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     """
 
     def do_GET(self):
+        print("getting")
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
